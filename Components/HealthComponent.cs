9addfee925417a74554f76130dfa81f943d31821
Daniel Hedrén <danielhedren@gmail.com>
2020-12-12 15:00:18 +0100

    IsDead convenience property

--- Components/HealthComponent.cs
+++ Components/HealthComponent.cs
@@ -4,6 +4,7 @@ namespace roguelike.Components
     {
         public double CurrentHealth { get; set; }
         public double MaxHealth { get; set; }
+        public bool IsDead { get => CurrentHealth <= 0; }
 
         public HealthComponent(double maxHealth)
         {
