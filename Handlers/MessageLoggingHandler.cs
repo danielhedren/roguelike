9ffd709d02e78e7b58e883bb3e88431e47d0998a
Daniel Hedrén <danielhedren@gmail.com>
2020-12-12 15:02:19 +0100

    Remove damage taken from messagelogging

--- Handlers/MessageLoggingHandler.cs
+++ Handlers/MessageLoggingHandler.cs
@@ -8,21 +8,15 @@ namespace roguelike.Handlers
     {
         public MessageLoggingHandler()
         {
-            EventBus.Subscribe(typeof(OnDamageTakenEvent), this);
             EventBus.Subscribe(typeof(OnAttackEvadedEvent), this);
         }
 
         public void HandleEvent(Event e, Level level)
         {
-            if (e.GetType() == typeof(OnDamageTakenEvent))
-            {
-                var ev = (OnDamageTakenEvent) e;
-
-                Logging.Log($"{ev.GetType()}: {ev.Attacker.GetType()} damaged {ev.Target.GetType()} for {ev.Damage} damage!");
-            } else if (e.GetType() == typeof(OnAttackEvadedEvent)) {
+            if (e.GetType() == typeof(OnAttackEvadedEvent)) {
                 var ev = (OnAttackEvadedEvent) e;
 
-                Logging.Log($"{ev.GetType()}: {ev.Target.GetType()} evaded {ev.Attacker.GetType()}s attack!");
+                Logging.Log($"{ev.GetType()}: {ev.IntendedTarget.GetType()} evaded {ev.Attacker.GetType()}s attack!");
             }
         }
     }
