a43882a5207e7266e1cb700239530d5e22556238
Daniel Hedrén <danielhedren@gmail.com>
2020-12-15 21:05:32 +0100

    Increase health on level up

--- Handlers/ExperienceHandler.cs
+++ Handlers/ExperienceHandler.cs
@@ -1,6 +1,7 @@
 using roguelike.Components;
 using roguelike.Engine;
 using roguelike.Events;
+using roguelike.Utils;
 
 namespace roguelike.Handlers
 {
@@ -9,24 +10,38 @@ namespace roguelike.Handlers
         public ExperienceHandler(World world) : base(world)
         {
             Subscribe(typeof(OnExperienceGainedEvent));
+            Subscribe(typeof(OnLevelGainedEvent));
         }
 
         public override void HandleEvent(Event e)
         {
-            var ev = (OnExperienceGainedEvent) e;
+            if (e.GetType() == typeof(OnExperienceGainedEvent))
+            {
+                var ev = (OnExperienceGainedEvent) e;
 
-            var experience = ev.Target.Get<ExperienceComponent>();
-            if (experience == null) return;
+                var experience = ev.Target.Get<ExperienceComponent>();
+                if (experience == null) return;
 
-            var level = experience.Level;
-            experience.Experience += ev.Experience;
+                var level = experience.Level;
+                experience.Experience += ev.Experience;
 
-            if (experience.Level != level) {
-                _world.EventBus.Publish(new OnLevelGainedEvent {
-                    Target = ev.Target,
-                    PreviousLevel = level,
-                    NewLevel = experience.Level
-                });
+                if (experience.Level != level) {
+                    _world.EventBus.Publish(new OnLevelGainedEvent {
+                        Target = ev.Target,
+                        PreviousLevel = level,
+                        NewLevel = experience.Level
+                    });
+                }
+            } else {
+                var ev = (OnLevelGainedEvent) e;
+
+                var health = ev.Target.Get<HealthComponent>();
+                var stats = ev.Target.Get<StatsComponent>();
+
+                if (health != null && stats != null) {
+                    health.MaxHealth += Random.Dice(1, stats.HitDice, stats.ConstitutionModifier);
+                    health.CurrentHealth = health.MaxHealth;
+                }
             }
         }
     }
