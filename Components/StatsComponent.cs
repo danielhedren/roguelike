4f31f19fe8691f7293d639c0c018ee19b8ff7e4c
Daniel Hedrén <danielhedren@gmail.com>
2021-09-14 21:24:58 +0200

    Clean up modifier calculation

--- Components/StatsComponent.cs
+++ Components/StatsComponent.cs
@@ -12,19 +12,24 @@ namespace roguelike.Components
             Charisma
         }
         public int Strength { get; set; }
-        public int StrengthModifier { get => (Strength - 10) / 2; }
+        public int StrengthModifier { get => Modifier(Strength); }
         public int Dexterity { get; set; }
-        public int DexterityModifier { get => (Dexterity - 10) / 2; }
+        public int DexterityModifier { get => Modifier(Dexterity); }
         public int Constitution { get; set; }
-        public int ConstitutionModifier { get => (Constitution - 10) / 2; }
+        public int ConstitutionModifier { get => Modifier(Constitution); }
         public int Intelligence { get; set; }
-        public int IntelligenceModifier { get => (Intelligence - 10) / 2; }
+        public int IntelligenceModifier { get => Modifier(Intelligence); }
         public int Wisdom { get; set; }
-        public int WisdomModifier { get => (Wisdom - 10) / 2; }
+        public int WisdomModifier { get => Modifier(Wisdom); }
         public int Charisma { get; set; }
-        public int CharismaModifier { get => (Charisma - 10) / 2; }
+        public int CharismaModifier { get => Modifier(Charisma); }
         public int HitDice { get; set; }
         public int ArmorClass { get; set; }
         public int ExperienceGained { get; set; }
+
+        private static int Modifier(int stat)
+        {
+            return (stat - 10) / 2;
+        }
     }
 }
